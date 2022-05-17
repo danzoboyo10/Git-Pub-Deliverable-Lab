841d23e068683eee04ebb9a3e81dd9d1b82ca615
Daniel Slebodnick <DanSlebodnick@Garys-MacBook-Pro.local>
2022-05-17 18:59:16 -0400

    /drinks is working on the server

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const drinks = require('./models/drinks');
+const fruits = require('./models/drinks');
 const port = 3000;
 
 const app = express();
 
 
-
-
 app.get('/', (req, res)=> {
-    res.send("Welcome to the Gitpub app" )
+    res.send("Welcome to the Gitpub app")
 
+})
+app.get('/drinks', (req, res)=> {
+    res.send(drinks)
 
 })
 
